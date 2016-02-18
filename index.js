99a0ca8dc7c38c8d7ab4aeb83e362188d9f4cdb7
steversj <Joshua.Steverson-Contractor@adp.com>
2016-02-17 21:59:44 -0500

    update lint rules

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ express or implied.  See the License for the specific language
 governing permissions and limitations under the License.
 */
 
+
 'use strict';
 
 var adpApiProduct = require('./lib/adpApiProduct');
